550dea38a359f7a45c4f71d4c4d424fd196ac38a
Demetrius Papas <papas.demetrius+git@gmail.com>
2024-08-11 12:05:23 +0000

    S1E11

--- main.go
+++ main.go
@@ -5,18 +5,24 @@ import (
 	"net/http"
 )
 
+func readinessHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
+
 func main() {
 	fmt.Println("Server booting...")
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
-	srv := &http.Server {
-		Addr: ":8080",
+	mux.Handle("/app/*", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", readinessHandler)
+	srv := &http.Server{
+		Addr:    ":8080",
 		Handler: mux,
 	}
+	fmt.Println("### SERVER RUNNING ###")
 	err := srv.ListenAndServe()
 	if err != nil {
 		fmt.Println("Error on ListenAndServe() call:", err)
 	}
-	fmt.Println("### Server running ###")
-
 }
