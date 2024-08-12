c00d36f85f0496e47584efebaa15b408634d8306
Demetrius Papas <papas.demetrius+git@gmail.com>
2024-08-12 15:27:59 +0000

    S2E03

--- main.go
+++ main.go
@@ -5,7 +5,31 @@ import (
 	"net/http"
 )
 
+type apiConfig struct {
+	fileserverHits int
+}
+
 func readinessHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) middlewareMectricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits)))
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits = 0
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(200)
 	w.Write([]byte("OK"))
@@ -13,9 +37,13 @@ func readinessHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	fmt.Println("Server booting...")
+	var apiCfg apiConfig
 	mux := http.NewServeMux()
-	mux.Handle("/app/*", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
-	mux.HandleFunc("/healthz", readinessHandler)
+	fServer := http.StripPrefix("/app/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/*", apiCfg.middlewareMectricsInc(fServer))
+	mux.HandleFunc("GET /healthz", readinessHandler)
+	mux.HandleFunc("GET /metrics", apiCfg.metricsHandler)
+	mux.HandleFunc("/reset", apiCfg.resetHandler)
 	srv := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
